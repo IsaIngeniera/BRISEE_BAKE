4683dca85248f151f81462f7dfbd0c1bb483fc15
Misja117 <juanhernandezmartelo117@gmail.com>
2026-09-02 13:31:42 -0500

    fix(lint): remove unused Get import in CarritoController

--- backend/src/carrito/carrito.controller.ts
+++ backend/src/carrito/carrito.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CarritoService } from './carrito.service';
 import { AddItemDto } from './dto/add-item.dto';
 import { ApiTags } from '@nestjs/swagger';
